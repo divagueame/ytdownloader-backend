72a737780343d1b8e12a5e5e976680f17e896b24
Martin Arce <martin.arce.teixeira@gmail.com>
2024-06-23 00:02:10 +0200

    Setup

--- main.go
+++ main.go
@@ -2,9 +2,27 @@ package main
 
 type song struct {
   Title string `json:"title"`
+import (
+	"context"
+
+	"encoding/json"
+	"fmt"
+	"github.com/lrstanley/go-ytdlp"
+	"log"
+	// "io"
+	"github.com/gorilla/websocket"
+	"net/http"
+	"time"
+	// "github.com/go-chi/chi/v5"
+	// "github.com/go-chi/chi/v5/middleware"
+)
+
 }
 
 var songs  = []song{
   {Title: "Chiki"},
   {Title: "Poin"}
+func main() {
+	fmt.Println("Hello, world!")
+}
 }
