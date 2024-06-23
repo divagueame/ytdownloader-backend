2c6c48443f8ec7ccef86df853f89ffb5e594c34b
Martin Arce <martin.arce.teixeira@gmail.com>
2024-06-23 09:08:54 +0200

    Add download package

---
+++ download.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+
+	"fmt"
+	"github.com/lrstanley/go-ytdlp"
+	"log"
+)
+
+func download(url string) {
+	fmt.Println("---> Downloading")
+	ytdlp.MustInstall(context.TODO(), nil)
+
+	dl := ytdlp.New().
+		ExtractAudio().
+		AudioFormat("mp3").
+		// FormatSort("res,ext:mp4:m4a").
+		// RecodeVideo("mp4").
+		Output("%(extractor)s - %(title)s.%(ext)s")
+
+	// _, err := dl.Run(context.TODO(), "https://www.youtube.com/watch?v=3lr9STd2Ryk")
+	download_res, err := dl.Run(context.TODO(), url)
+	if err != nil {
+
+		log.Println("Error donwloading:", err)
+		// panic(err)
+	} else {
+		log.Println("---> Downloading DONE!", download_res)
+	}
+}
