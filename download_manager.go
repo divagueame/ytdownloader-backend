4b109aa618fdb018fceb46a4ae14f5ba2cad5acd
Martin Arce <martin.arce.teixeira@gmail.com>
2024-06-26 01:37:12 +0200

    Add Download struct

--- download_manager.go
+++ download_manager.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/lrstanley/go-ytdlp"
 	"log"
 	"sync"
 )
 
+type Download struct {
+	Id       string
+	State    State
+	Channel  chan State `json:"-"`
+	Owner    string
+	FilePath string
+	Url      string `json:"url"`
+}
+
 type DownloadManager struct {
-	downloads map[string]chan bool
-	mu        sync.Mutex
+	downloads map[string]Download
+	mu sync.Mutex
 }
 
 func NewDownloadManager() *DownloadManager {
 	return &DownloadManager{
-		downloads: make(map[string]chan bool),
+		downloads: make(map[string]Download),
 	}
 }
 
