b8e8437c4d5f22d0f73381e65859b999df6b7d4a
Martin Arce <martin.arce.teixeira@gmail.com>
2024-06-26 15:11:47 +0200

    Store references to Download in DownloadManager map

--- download_manager.go
+++ download_manager.go
@@ -18,16 +18,31 @@ type Download struct {
 }
 
 type DownloadManager struct {
-	downloads map[string]Download
-	mu sync.Mutex
+	downloads map[string]*Download
+	mu        sync.Mutex
 }
 
 func NewDownloadManager() *DownloadManager {
 	return &DownloadManager{
-		downloads: make(map[string]Download),
+		downloads: make(map[string]*Download),
 	}
 }
 
+func (dm *DownloadManager) createDownload(url string, session_id string) *Download {
+
+	download := &Download{
+		Id:      uuid.New().String(),
+		State:   Idle,
+		Channel: make(chan State),
+		Owner:   session_id,
+		Url:     url,
+	}
+
+	dm.mu.Lock()
+	dm.downloads[download.Id] = download
+	dm.mu.Unlock()
+	return download
+}
 func (dm *DownloadManager) logDownloads() {
 	log.Print("Current Downloads")
 	for key := range dm.downloads {
