fca4f048b72e41939ea93639315363b9b45fd65d
Martin Arce <martin.arce.teixeira@gmail.com>
2024-06-26 15:10:21 +0200

    Basic routes

--- main.go
+++ main.go
@@ -17,12 +17,41 @@ import (
 	// "github.com/go-chi/chi/v5/middleware"
 )
 
+var (
+	// addr      = flag.String("addr", ":8080", "http service address")
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
 }
 
-var songs  = []song{
-  {Title: "Chiki"},
-  {Title: "Poin"}
-func main() {
-	fmt.Println("Hello, world!")
+type State int
+
+const (
+	Idle = iota
+	Processing
+	Done
+	Error
+)
+
+type DownloadMsg struct {
+	Id    string `json:"id"`
+	State State  `json:"state"`
+	Url   string `json:"url"`
 }
+
+func main() {
+	// go logDownloads()
+	http.HandleFunc("/mp3/", serveFile)
+	http.HandleFunc("/", setSessionCookieReq)
+	http.HandleFunc("/ws", serveWs)
+
+	http.HandleFunc("/download", triggerDownload)
+	// go logDownloads()
+	http.ListenAndServe(":5000", nil)
+	// http.HandleFunc("/", serveHome)
+}
+
 }
