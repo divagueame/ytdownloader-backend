1a91f69b2d397ee8badf2ac9ab2c5981a7aa730e
Martin Arce <martin.arce.teixeira@gmail.com>
2024-06-29 23:04:00 +0200

    Refactor cookie provider

--- cookie.go
+++ cookie.go
@@ -6,14 +6,45 @@ import (
 	"time"
 )
 
-func setSessionCookie(w http.ResponseWriter, r *http.Request) {
-	_, err := r.Cookie("puki")
-	if err != nil && err == http.ErrNoCookie {
-		user_id_cookie := http.Cookie{
-			Name:    "puki",
-			Value:   uuid.New().String(),
-			Expires: time.Now().Add(24 * time.Hour),
+func setSessionCookie(w http.ResponseWriter, r *http.Request) http.Cookie {
+	session_id, session_err := r.Cookie("chikiyt_session_id")
+
+	if session_err != nil {
+		if session_err == http.ErrNoCookie {
+			session_id = &http.Cookie{
+				Name:     "chikiyt_session_id",
+				Value:    uuid.New().String(),
+				Expires:  time.Now().Add(24 * time.Hour),
+				Path:     "/",
+				SameSite: http.SameSiteLaxMode,
+			}
+			http.SetCookie(w, session_id)
 		}
-		http.SetCookie(w, &user_id_cookie)
 	}
+
+	return *session_id
+
+	// cookie, err := r.Cookie("chikiyt_session_id")
+	//
+	// if err != nil && err == http.ErrNoCookie {
+	// 	cookie := http.Cookie{
+	// 		Name:    "chikiyt_session_id",
+	// 		Value:   uuid.New().String(),
+	// 		Expires: time.Now().Add(24 * time.Hour),
+	// 	}
+	// 	http.SetCookie(w, &cookie)
+	// 	return cookie
+	// } else {
+	// 	return *cookie
+	// }
 }
+
+//
+// func readSessionCookieValue(r *http.Request) string {
+// 	cookie, err := r.Cookie("chikiyt_session_id")
+// 	if err != nil && err == http.ErrNoCookie {
+// 		return ""
+//
+// 	}
+// 	return cookie.Value
+// }
